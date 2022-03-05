18e0e7d8e92065c0ee2daebabc08bed96a3d6a0b
seanprehensible <seanprehensible@gmail.com>
2022-03-05 17:56:33 +0900

    Add Boilerplate Code except webpack-dev-server

---
+++ webpack.config.ts
@@ -0,0 +1,122 @@
+import path from 'path';
+// import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import webpack, { Configuration as WebpackConfiguration } from 'webpack';
+// import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+// import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+// import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
+
+interface Configuration extends WebpackConfiguration {
+  // devServer?: WebpackDevServerConfiguration;
+}
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+const config: Configuration = {
+  name: 'slack-front-setup',
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: !isDevelopment ? 'hidden-source-map' : 'inline-source-map',
+  /* 입력 */
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    alias: {
+      '@hooks': path.resolve(__dirname, 'hooks'),
+      '@components': path.resolve(__dirname, 'components'),
+      '@layouts': path.resolve(__dirname, 'layouts'),
+      '@pages': path.resolve(__dirname, 'pages'),
+      '@utils': path.resolve(__dirname, 'utils'),
+      '@typings': path.resolve(__dirname, 'typings'),
+    },
+  },
+  entry: {
+    app: './client',
+  },
+
+  target: ['web', 'es5'] /* 웹 플랫폼용 런타임 코드를 생성하고 ES5 기능만 사용 */,
+
+  /* 적용할 모듈 */
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: { browsers: ['IE 10'] },
+                debug: isDevelopment,
+              },
+            ],
+            '@babel/preset-react',
+            '@babel/preset-typescript',
+          ],
+          // env: {
+          //   development: {
+          //     plugins: [
+          //       ['@emotion/babel-plugin', { sourceMap: true }],
+          //       require.resolve('react-refresh/babel'),
+          //     ],
+          //   },
+          //   production: {
+          //     plugins: ['@emotion/babel-plugin'],
+          //   },
+          // },
+        },
+      },
+      {
+        /* CSS도 JavaScript 파일로 변환 */
+        test: /\.css?$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  /* 확장 프로그램 */
+  plugins: [
+    // new ForkTsCheckerWebpackPlugin({
+    //   async: false,
+    //   // eslint: {
+    //   //   files: "./src/**/*",
+    //   // },
+    // }),
+    // process.env.NODE_ENV를 프론트엔드에서 접근 가능하도록 설정
+    new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }),
+  ],
+  /* 출력 */
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js' /* entry에서 설정된 이름인 app으로 변경됨(app.js) */,
+    publicPath: '/dist/',
+  },
+  /* 웹팩 데브 서버 */
+  // devServer: {
+  //   historyApiFallback: true,
+  //   port: 3090,
+  //   devMiddleware: { publicPath: '/dist/' },
+  //   static: { directory: path.resolve(__dirname) },
+  //   proxy: {
+  //     '/api/': {
+  //       target: 'http://localhost:3095',
+  //       changeOrigin: true,
+  //       ws: true,
+  //     },
+  //   },
+  // },
+};
+
+// if (isDevelopment && config.plugins) {
+//   config.plugins.push(new webpack.HotModuleReplacementPlugin());
+//   config.plugins.push(
+//     new ReactRefreshWebpackPlugin({
+//       overlay: {
+//         useURLPolyfill: true,
+//       },
+//     })
+//   );
+//   config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'server', openAnalyzer: false }));
+// }
+// if (!isDevelopment && config.plugins) {
+//   config.plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
+//   config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'static' }));
+// }
+
+export default config;
