e7fbafd7f9bce10b2c81e38aa3ed3335d6b8f546
seanprehensible <seanprehensible@gmail.com>
2022-03-06 11:22:11 +0900

    Add pages with router

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import LogIn from '@pages/LogIn';
+import SignUp from '@pages/SignUp';
 
-const App = () => {
-  return <div>App Test</div>;
+const App: FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<LogIn />} />
+      <Route path="/login" element={<LogIn />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  );
 };
 
 export default App;
